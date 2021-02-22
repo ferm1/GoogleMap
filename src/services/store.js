22ee81dda657844b27dc611cf220cf053d7d51c4
Fermin Gicangao <fermingicangao26@gmail.com>
2021-02-22 12:40:43 +0800

    FERMIN_GICANGAO: Store Service

---
+++ src/services/store.js
@@ -0,0 +1,78 @@
+export const getStores = () => {
+    return Promise.resolve ({
+        data: [
+            {
+                id: 1,
+                name: 'Gaisano Grand Mandaue North',
+                address:'Mandaue City, Cebu',
+                tags:'Shopping Mall,Hardware,Restaurant,Grocery,Super Market',
+                latitude: 10.3567106,
+                longitude: 123.9480096,
+            },
+            {
+                id: 2,
+                name: "Quim's Savor",
+                address:'Mandaue City, Cebu',
+                tags:'Restaurant',
+                latitude: 10.3629548,
+                longitude: 123.9546621,
+            },
+            {
+                id: 3,
+                name: 'Tuslob Buwa Sa Gobiao',
+                address:'Acacia Hills Subdivision Red, Mandaue City, 6014 Cebu',
+                tags:'Restaurant',
+                latitude: 10.3655806,
+                longitude: 123.9578584,
+            },
+            {
+                id: 4,
+                name: 'Dave Housing Supply',
+                address:'Purok 2 Cubacub Road, Cubacub St, Mandaue City, 6014 Cebu',
+                tags:'Hardware',
+                latitude: 10.3662312,
+                longitude: 123.9457961,
+            },
+            {
+                id: 5,
+                name: 'Triple J Bakeshop',
+                address:'Ibabaw, Tigbongan, Cosolation, 6001 Metro Manila',
+                tags:'Bakery',
+                latitude: 10.3650988,
+                longitude: 123.965642,
+            },
+             {
+                id: 6,
+                name: "JJ's Chika-an",
+                address:'Capt. Flordelis, Street, Hilongos, Leyte',
+                tags:'Restaurant',
+                latitude: 10.3766797,
+                longitude: 124.7467863,
+            },
+            {
+                id: 7,
+                name: 'LBC Express',
+                address:'Rv Villaflores Corner Rv Fulache Street, Hilongos City, 6524 Leyte',
+                tags:'Transportation Service',
+                latitude: 10.3757088,
+                longitude: 124.7491681,
+            },
+            {
+                id: 8,
+                name: 'Tambaan Farm Cafe',
+                address:'Sitio Tambaan, Hilongos, Leyte',
+                tags:'Restaurant',
+                latitude: 10.3805091,
+                longitude: 124.7501495,
+            },
+            {
+                id: 9,
+                name: 'Go Resort',
+                address:'Brgy. Liberty, Hilongos, Leyte',
+                tags:'Resort',
+                latitude: 10.3957367,
+                longitude: 124.7579587,
+            },
+        ],
+    });
+};
